15e962f3daa607fe10277fea6e3ddc10322f8f34
Edgard Vilo <evilo@hostyweb.cl>
2022-01-23 11:54:40 -0300

    Clase 15: Realizar animación cuando se llega al valor máximo

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
 
@@ -16,12 +17,17 @@ export const CounterEffect = () => {
 
         console.log('%cSe llego al valor máximo', 'color: red; background-color: black');
 
+        gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
+            gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' });
+        });
+
     }, [counter]);
     
 
     return(
         <>
-            <h1>Counter Effect: { counter }</h1>
+            <h1>Counter Effect:</h1>
+            <h2>{ counter }</h2>
 
             <button onClick={ handleClick }>
                 +1
