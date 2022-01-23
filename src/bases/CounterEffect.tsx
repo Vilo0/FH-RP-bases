548c3a4abcfce238300fe2067ea661381f9247bd
Edgard Vilo <evilo@hostyweb.cl>
2022-01-23 12:18:32 -0300

    Clase 16: useRef y TimeLines

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
@@ -6,6 +6,7 @@ const MAXIMUN_COUNT = 10;
 export const CounterEffect = () => {
 
     const [counter, setCounter] = useState(5);
+    const counterElement = useRef<HTMLHeadingElement>(null);
 
     const handleClick = () => {
         setCounter( prev => Math.min(prev + 1, MAXIMUN_COUNT) );
@@ -17,17 +18,17 @@ export const CounterEffect = () => {
 
         console.log('%cSe llego al valor máximo', 'color: red; background-color: black');
 
-        gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
-            gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' });
-        });
+        const timeline = gsap.timeline();
 
+        timeline.to( counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' } )
+                .to( counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' } );
     }, [counter]);
     
 
     return(
         <>
             <h1>Counter Effect:</h1>
-            <h2>{ counter }</h2>
+            <h2 ref={ counterElement }>{ counter }</h2>
 
             <button onClick={ handleClick }>
                 +1
